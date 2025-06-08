a403b6f63f127b51dcc128dfd3b9eeb1860dd4e0
Carla Lopez <lpz.carla12@gmail.com>
2025-06-08 00:03:20 -0300

    video/audio/subir imagenes/subir a servidor backend/subir archivos/apuntes en código

--- js/camara.js
+++ js/camara.js
@@ -1,2 +1,38 @@
 const picture = document.querySelector("div.container img")
-const filePicture = document.querySelector("input[type=file]")
+const botonCamera = document.querySelector("button")
+//const filePicture = document.querySelector("input[type=file]")
+//es igual que lo siguiente, pero lo siguiente está mejor enlasado
+const filePicture = document.createElement("input")
+//este es el elemento html que cambiamos a js
+filePicture.type = "file"
+filePicture.id = "inputCamera"
+filePicture.cature = "environment"
+filePicture.accept = ".png, .jpg, .webp, .gif" 
+
+botonCamera.addEventListener("click", ()=> filePicture.click())
+
+filePicture.addEventListener("change", ()=> {
+    console.clear()
+    console.log(filePicture.files[0])
+
+    const objectURL = URL.createObjectURL(filePicture.files[0])
+    picture.src= objectURL
+    console.log(objectURL)
+
+    //CONVERTIR A BASE64
+    //para subir un archivo a un servidor web (de backend)
+    const reader = new FileReader()
+    reader.readAsDataURL(filePicture.files[0])
+
+    reader.addEventListener("error", ()=> {
+        console.log("Se produjo un error inesperado", error)
+    })
+
+    reader.addEventListener("load", ()=> {
+        const base64String = reader.result
+        console.log(base64String)
+    })
+
+    
+
+})
--- js/index.js
+++ js/index.js
@@ -5,5 +5,51 @@ const videos = [
 ]
 
 const video = document.querySelector("video")
-const videoName = document.querySelector("p#videoName")
+const videoTag = document.querySelector("p#videoName")
 
+function cargarVideo() {
+    const speed = navigator.connection.effectiveType
+    const media = videos.find((video) => video.speed === speed)
+
+    videoTag.textContent = media.tag
+    video.src = media.video
+    //video.play()
+}
+
+
+
+cargarVideo()
+
+navigator.connection.addEventListener("change", ()=> {
+    console.log(navigator.connection.effectiveType)
+})
+
+navigator.connection.addEventListener("change", ()=> {
+    cargarVideo()
+})
+
+video.addEventListener("timeupdate", () => {
+    console.log(video.currentTime) //con esto podríamos controlar en que momento del video la conección mejora
+})
+
+// //const audio = document.createElement("audio")
+// const audio = new Audio()
+
+// audio.src = "media/mi-musica.mp3"
+// audio.currentTime //nos permite ver el tiempo transcurrido en segundos
+// audio.play()
+// audio.pause()
+// audio.addEventListener("timeupdate", () => {
+//     //obtenerTiempo(audio.currentTime) // convierte segs. en hh:mm:ss
+// })
+// audio.addEventListener("play", ()=> {
+//     //manejar eventos de play
+// })
+
+// audio.addEventListener("pause", ()=> {
+//     //manejar eventos de pausa
+// })
+
+// audio.addEventListener("playing", () => {
+//     //detectar si se está reproduciendo un audio
+// })
